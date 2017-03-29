1a1ff6e62b33e968e20864f1cec1485246f44e84
lujianmei <anysky130@163.com>
2017-03-29 10:06:53 +0800

    remove org-html-themes

---
+++ layers/init-programming/config.el
@@ -0,0 +1,12 @@
+;;; config.el --- lujianmei Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2015-2016 lujianmei
+;;
+;; Author: lujianmei
+;; URL: https://github.com/lujianmei/.spacemacs.d.git
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+
---
+++ layers/init-programming/funcs.el
@@ -0,0 +1,11 @@
+;;; funcs.el --- lujianmei Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2015-2016 lujianmei
+;;
+;; Author: lujianmei
+;; URL: https://github.com/lujianmei/.spacemacs.d.git
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
