b0e9386b81d33c4bc29531b1bee3d7a576ae3c48
lujianmei <anysky130@163.com>
2018-07-22 20:22:45 +0800

    changed

--- third-plugins/mu4e/mu4e-meta.el
+++ third-plugins/mu4e/mu4e-meta.el
@@ -1,11 +1,11 @@
 ;; auto-generated
-(defconst mu4e-mu-version "0.9.18"
+(defconst mu4e-mu-version "1.0"
   "Required mu binary version; mu4e's version must agree with this.")
 
-(defconst mu4e-builddir "/usr/local/Cellar/mu/0.9.18_1/"
+(defconst mu4e-builddir "/home/djcb/Sources/mu"
   "Top-level build directory.")
 
-(defconst mu4e-doc-dir "/usr/local/share/doc/mu/"
+(defconst mu4e-doc-dir "/usr/local/share/doc/mu"
   "Mu4e's data-dir.")
 
 (provide 'mu4e-meta)
